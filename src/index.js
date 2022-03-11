2fc503d2347c137e928be762c9d9705aa600e632
wgt75 <mark11seM>
2022-03-11 19:06:11 +0700

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "firebase/app"
-import { getFirestore, collection, getDocs } from "firebase/firestore"
+import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc, setDoc } from "firebase/firestore"
 
 const firebaseConfig = {
     apiKey: "AIzaSyCrdaAfxVMq4MspXe6cdLWvnZoAiLM-JtU",
@@ -10,14 +10,15 @@ const firebaseConfig = {
     messagingSenderId: "86436915855",
     appId: "1:86436915855:web:9f007cc77db42a8a6a9efa"
 }
-
+//npm install firebase
+// npm i webpack webpack-cli -D
 //npm run build
 
 initializeApp(firebaseConfig)
 
 const db = getFirestore()
 
-const colRef = collection(db, 'books')
+const colRef = collection(db, 'buttons')
 
 getDocs(colRef)
     .then((snapshot) => {
@@ -31,3 +32,40 @@ getDocs(colRef)
         console.log(err.message)
     })
 
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addDoc(colRef, {
+        title: addBookForm.title.value,
+        author: addBookForm.author.value,
+    })
+        .then(() => {
+            addBookForm.reset()
+        })
+})
+
+// deleting docs
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const docRef = doc(db, 'books', deleteBookForm.id.value)
+
+    deleteDoc(docRef)
+        .then(() => {
+            deleteBookForm.reset()
+        })
+})
+
+
+// const a = ["10", "12", "14", "16", "18"]
+// const b = ["pn", "vt", "sr", "cht", "pt", "sb", "vs"]
+//
+// for (let i = 0; i < a.length; i++) {
+//     for (let j = 0; j < b.length; j++) {
+//         addDoc(colRef, {
+//             value: "",
+//             active: true,
+//         }, a[i] + b[j])
+// }}
